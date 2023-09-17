2844010133a5abe987fec96da44e67885eae3838
liusen <liusen@huice.com>
2023-09-17 10:26:48 +0800

    记录请求的POST请求体

--- gateway/src/main/java/com/carsonlius/filter/RequestTimeGatewayFilterFactory.java
+++ gateway/src/main/java/com/carsonlius/filter/RequestTimeGatewayFilterFactory.java
@@ -6,6 +6,8 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
@@ -48,10 +50,18 @@ public class RequestTimeGatewayFilterFactory extends AbstractGatewayFilterFactor
                     Long startTime = exchange.getAttribute(REQUEST_TIME_BEGIN);
                     StringBuilder logStr = new StringBuilder("耗时统计 " + exchange.getRequest().getURI().getPath());
                     logStr.append(":").append((System.currentTimeMillis() - startTime)).append("ms");
-                    if (config.isWithParams()) {
-                        logStr.append(" 入参: ").append(exchange.getRequest().getQueryParams()).append(" post入参:").append(JsonUtils.toJson(exchange.getRequest().getBody()));
-                    }
 
+                    if (config.isWithParams()) {
+                        logStr.append(" 入参: ").append(exchange.getRequest().getQueryParams());
+                        if (exchange.getRequest().getMethod().equals(HttpMethod.POST)){
+                            GatewayContext gatewayContext = exchange.getAttribute(GatewayContext.CACHE_GATEWAY_CONTEXT);
+                            if(exchange.getRequest().getHeaders().getContentType().equals(MediaType.APPLICATION_JSON)){
+                                logStr.append(" POST参数:").append(gatewayContext.getJsonBody());
+                            } else {
+                                logStr.append(" POST参数:").append(JsonUtils.toJson(gatewayContext.getMultiPartParams()));
+                            }
+                        }
+                    }
                     log.info(logStr.toString());
                 })));
             }
